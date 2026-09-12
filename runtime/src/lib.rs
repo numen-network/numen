e6a222d00dc2c516ac0043395101eb1c953e4996
motoZ-crypto <motoz.crypto@proton.me>
2026-09-12 16:02:45 +0000

    bump node to 0.4.0 and spec_version to 10000

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -68,12 +68,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("numen-runtime"),
 	impl_name: alloc::borrow::Cow::Borrowed("numen-runtime"),
 	authoring_version: 1,
-	// The version of the runtime specification. A full node will not attempt to use its native
-	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-	//   the compatible custom types.
-	spec_version: 100,
+	spec_version: 10000,
 	impl_version: 1,
 	apis: apis::RUNTIME_API_VERSIONS,
 	transaction_version: 1,
